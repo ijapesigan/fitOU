49ec1524aeb5a72fd971dfd78d6f92b3d42cd9d1
ijapesigan <ijapesigan@users.noreply.github.com>
2023-10-29 23:19:52 +0000

    Automated build.
    Files changed:
    A	.Rbuildignore
    A	.github/linters/.lintr
    A	.setup/build/fitOU.pdf
    A	.setup/build/fitOU_0.0.0.9000.tar.gz
    A	.setup/latex/bib/quarto.bib
    A	.setup/lint/.lintr
    A	.setup/quarto/bib/quarto.bib
    A	CITATION.cff
    M	DESCRIPTION
    A	LICENSE.md
    A	NAMESPACE
    M	R/fitOU-fit-ou-id.R
    M	R/fitOU-fit-ou.R
    A	README.md
    A	data/bivariate_ou.rda
    A	man/FitOU.Rd
    A	man/FitOUID.Rd
    A	man/bivariate_ou.Rd
    A	man/fitOU-package.Rd
    M	project.Rproj
    A	vignettes/bivariate-ou.Rmd
    A	vignettes/sim-bivariate-ou.Rmd
    A	vignettes/trivariate-ou.Rmd
    A	vignettes/vignettes.bib

--- R/fitOU-fit-ou.R
+++ R/fitOU-fit-ou.R
@@ -10,7 +10,7 @@
 #'   \boldsymbol{\nu}
 #'   +
 #'   \boldsymbol{\Lambda}
-#'   \boldsymbol{\eta}_{i, t} 
+#'   \boldsymbol{\eta}_{i, t}
 #'   +
 #'   \boldsymbol{\varepsilon}_{i, t}
 #'   \quad
@@ -69,7 +69,7 @@
 #' which represents random fluctuations.
 #'
 #' @author Ivan Jacob Agaloos Pesigan
-#' 
+#'
 #' @param data Data frame.
 #'   A data frame object of data for potentially
 #'   multiple subjects that contain
@@ -264,7 +264,7 @@ FitOU <- function(data,
   }
   names(mu_start) <- mu_names
   if (is.null(phi_start)) {
-    phi_start <- rep(x = 0, times = k * k)  
+    phi_start <- rep(x = 0, times = k * k)
   } else {
     dim(phi_start) <- NULL
   }
@@ -327,7 +327,7 @@ FitOU <- function(data,
     measurement = dynr_measurement,
     dynamics = dynr_dynamics,
     noise = dynr_noise,
-    outfile = paste0(tempfile(),".c")
+    outfile = paste0(tempfile(), ".c")
   )
   if (!is.null(lb)) {
     model$lb[phi_names] <- lb
